68ae18f308572b9a2112d3f4c662f4cc2f9fe2fe
clean-command-1851 <ed7901979t@gmail.com>
2019-08-13 01:38:23 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(my_string="Squawk!")
+  puts "#{my_string}"
+  return my_string
+end
+parrot()
+parrot(my_string="Hello")
